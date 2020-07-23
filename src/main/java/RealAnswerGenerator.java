08d83ff32b549a15724c38b8cd10eed27579b580
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-23 20:28:25 +0800

    RealAnswerGenerator [Jim] feature:implementation RealAnswerGenerator.generate

--- src/main/java/RealAnswerGenerator.java
+++ src/main/java/RealAnswerGenerator.java
@@ -1,7 +1,20 @@
-public class RealAnswerGenerator implements AnswerGenerator{
+import java.util.Random;
+
+public class RealAnswerGenerator implements AnswerGenerator {
     @Override
     public int[] generate() {
-        int[] answer = {1,3,2,10};
+        int[] answer = new int[4];
+        int n = 10;
+        Random rand = new Random();
+        boolean[] bool = new boolean[n];
+        int randInt = 0;
+        for (int i = 0; i < 4; i++) {
+            do {
+                randInt = rand.nextInt(n);
+            } while (bool[randInt]);
+            bool[randInt] = true;
+            answer[i] = randInt;
+        }
         return answer;
     }
 }
