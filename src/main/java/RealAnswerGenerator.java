919941b0bf037b2bfd39ad557920925dd7079cc5
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-23 19:59:21 +0800

    InputLegal [Jim] test:should_return_true_when_generate_answer_given_no_input_test_is_repeated

---
+++ src/main/java/RealAnswerGenerator.java
@@ -0,0 +1,7 @@
+public class RealAnswerGenerator implements AnswerGenerator{
+    @Override
+    public int[] generate() {
+        int[] answer = {1,1,2,3};
+        return answer;
+    }
+}
---
+++ src/test/java/RealAnswerGeneratorTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RealAnswerGeneratorTest {
+    @Test
+    public void should_return_true_when_generate_answer_given_no_input_test_is_repeated() {
+        RealAnswerGenerator realAnswerGenerator = new RealAnswerGenerator();
+        int[] answer = realAnswerGenerator.generate();
+
+        boolean result = true;
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = i + 1; j < answer.length; j++) {
+                if (answer[j] == answer[i]) {
+                    result = false;
+                }
+            }
+        }
+
+        Assertions.assertTrue(result);
+    }
+}
