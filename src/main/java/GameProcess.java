bfa53381a19543c7a8b56011b2ded674e5d1c464
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-23 21:37:20 +0800

    GameProcess [Jim] feature:should_return_wrong_string_and_time_is_1_when_guess_given_12

--- src/main/java/GameProcess.java
+++ src/main/java/GameProcess.java
@@ -1,8 +1,11 @@
 public class GameProcess {
     private GuessNumber guessNumber;
+    private InputLegal inputLegal;
     private int times;
+
     public GameProcess() {
         AnswerGenerator answerGenerator = new RealAnswerGenerator();
+        this.inputLegal = new InputLegal();
         this.guessNumber = new GuessNumber(answerGenerator);
         this.times = 0;
     }
@@ -12,6 +15,20 @@ public class GameProcess {
     }
 
     public String guess(int[] inputNumber) {
+        boolean isLegal = inputLegal.isLegal(inputNumber);
+        if (!isLegal) {
+            String result = "";
+            for (int i = 0; i < inputNumber.length; i++) {
+                result += inputNumber[i];
+                if (i == inputNumber.length - 1) {
+                    result += "\t";
+                } else {
+                    result += " ";
+                }
+            }
+            result += "Wrong Input, Input again\n";
+            return result;
+        }
         return "";
     }
 }
