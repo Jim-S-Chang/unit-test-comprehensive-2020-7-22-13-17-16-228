a9c4a1f1235f4ca5f66a691483ec16dfd0c87245
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-23 19:23:02 +0800

    InputLegal [Jim] feature:should_return_false_when_judge_input_legal_given_1123

--- src/main/java/InputLegal.java
+++ src/main/java/InputLegal.java
@@ -3,6 +3,18 @@ public class InputLegal {
     }
 
     public boolean isLegal(int[] inputNumbers) {
+        boolean isNoRepeatedNumbers = isRepeated(inputNumbers);
+        return isNoRepeatedNumbers;
+    }
+
+    private boolean isRepeated(int[] inputNumbers) {
+        for (int i = 0; i < inputNumbers.length; i++) {
+            for (int j = i + 1; j < inputNumbers.length; j++) {
+                if (inputNumbers[j] == inputNumbers[i]) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 }
