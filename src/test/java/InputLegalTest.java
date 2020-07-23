1cbfaa749038e8ab108af6ccf3df5962aace85bb
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-23 19:30:11 +0800

    InputLegal [Jim] test:should_return_false_when_judge_input_legal_given_12310

--- src/test/java/InputLegalTest.java
+++ src/test/java/InputLegalTest.java
@@ -21,4 +21,14 @@ public class InputLegalTest {
 
         Assertions.assertFalse(isLegal);
     }
+
+    @Test
+    void should_return_false_when_judge_input_legal_given_12310() {
+        int[] inputNumbers = {1, 2, 3, 10};
+        InputLegal inputLegal = new InputLegal();
+
+        boolean isLegal = inputLegal.isLegal(inputNumbers);
+
+        Assertions.assertFalse(isLegal);
+    }
 }
