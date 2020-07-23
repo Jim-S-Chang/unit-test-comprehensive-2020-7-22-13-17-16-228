695901eed3fb3091cb2e00fc1c344696350be5ec
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-23 18:49:06 +0800

    GuessNumber [Jim] test&feature:should_return_2A0B_when_guess_number_given_answer_1234_inputGuess_1278

--- src/test/java/GuessNumberTest.java
+++ src/test/java/GuessNumberTest.java
@@ -32,4 +32,18 @@ public class GuessNumberTest {
 
         Assertions.assertEquals("0A0B", result);
     }
+
+    @Test
+    public void should_return_2A0B_when_guess_number_given_answer_1234_inputGuess_1278() {
+        int[] answer = {1,2,3,4};
+        int[] inputGuess = {1,2,7,8};
+
+        AnswerGenerator answerGenerator = Mockito.mock(AnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(answer);
+        GuessNumber guessNumber = new GuessNumber(answerGenerator);
+
+        String result = guessNumber.guess(inputGuess);
+
+        Assertions.assertEquals("2A0B", result);
+    }
 }
