faaaafed83fb1466bb8e454b0dcb2f147e0d0bd7
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-23 19:34:06 +0800

    InputLegal [Jim] feature:should_return_false_when_judge_input_legal_given_12310

--- src/main/java/InputLegal.java
+++ src/main/java/InputLegal.java
@@ -5,7 +5,17 @@ public class InputLegal {
     public boolean isLegal(int[] inputNumbers) {
         boolean isNoRepeatedNumbers = isRepeated(inputNumbers);
         boolean isLengthLegal = judgeLengthLegal(inputNumbers);
-        return isNoRepeatedNumbers && isLengthLegal;
+        boolean isRangeLegal = judgeRange(inputNumbers);
+        return isNoRepeatedNumbers && isLengthLegal && isRangeLegal;
+    }
+
+    private boolean judgeRange(int[] inputNumbers) {
+        for (int i = 0; i < inputNumbers.length; i++) {
+            if (inputNumbers[i] < 0 || inputNumbers[i] > 9) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private boolean judgeLengthLegal(int[] inputNumbers) {
