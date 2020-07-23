e0f06f20a96b8acc425ea801c3929407d7989eb9
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-23 18:45:41 +0800

    GuessNumber [Jim] feature:should_return_4A0B_when_guess_number_given_answer_1234_inputGuess_1234

--- src/main/java/GuessNumber.java
+++ src/main/java/GuessNumber.java
@@ -1,8 +1,23 @@
 public class GuessNumber {
+    private int[] answer;
+
     public GuessNumber(AnswerGenerator answerGenerator) {
+        this.answer = answerGenerator.generate();
     }
 
     public String guess(int[] inputGuess) {
-        return null;
+        int countOfA = getCountOfA(inputGuess, this.answer);
+        String result = countOfA + "A0B";
+        return result;
+    }
+
+    private int getCountOfA(int[] inputGuess, int[] answer) {
+        int result = 0;
+        for (int i = 0; i < inputGuess.length; i++) {
+            if (inputGuess[i] == answer[i]) {
+                result++;
+            }
+        }
+        return result;
     }
 }
