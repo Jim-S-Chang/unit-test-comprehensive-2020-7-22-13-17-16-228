d4ec42fd6c820468c4890ef45693e0b3aa657293
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-23 21:19:01 +0800

    GameProcess [Jim] feature:should_return_output_strings_when_start_game_given_start GameProcess constructor

--- src/main/java/GameProcess.java
+++ src/main/java/GameProcess.java
@@ -1,6 +1,13 @@
 public class GameProcess {
+    private GuessNumber guessNumber;
+    private int times;
+    public GameProcess() {
+        AnswerGenerator answerGenerator = new RealAnswerGenerator();
+        this.guessNumber = new GuessNumber(answerGenerator);
+        this.times = 0;
+    }
 
     public String start() {
-        return "";
+        return "Input\tOutput\t";
     }
 }
