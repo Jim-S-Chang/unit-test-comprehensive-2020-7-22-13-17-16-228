2f04b49d7746b30246795775ae947aa8ac190468
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-23 22:11:28 +0800

    GameProcess [Jim] test:should_return_end_when_times_change_to_6_given_1243_with_answer1234

--- src/test/java/GameProcessTest.java
+++ src/test/java/GameProcessTest.java
@@ -28,29 +28,51 @@ public class GameProcessTest {
 
     @Test
     public void should_return_2A2B_and_time_is_1_when_guess_given_1243_with_answer1234() {
-        int[] answer = {1,2,3,4};
+        int[] answer = {1, 2, 3, 4};
         AnswerGenerator answerGenerator = Mockito.mock(AnswerGenerator.class);
         when(answerGenerator.generate()).thenReturn(answer);
         GameProcess gameProcess = new GameProcess(answerGenerator);
 
-        int[] inputNumber = {1, 2,4,3};
+        int[] inputNumber = {1, 2, 4, 3};
 
         String result = gameProcess.guess(inputNumber);
-        Assertions.assertEquals(gameProcess.getTimes(), 1);
+        Assertions.assertEquals(1, gameProcess.getTimes());
         Assertions.assertEquals("1 2 4 3\t2A2B\n", result);
     }
 
     @Test
     public void should_return_4A0B_and_times_change_to_6_when_guess_given_1234_with_answer1234() {
-        int[] answer = {1,2,3,4};
+        int[] answer = {1, 2, 3, 4};
         AnswerGenerator answerGenerator = Mockito.mock(AnswerGenerator.class);
         when(answerGenerator.generate()).thenReturn(answer);
         GameProcess gameProcess = new GameProcess(answerGenerator);
 
-        int[] inputNumber = {1, 2,4,3};
+        int[] inputNumber = {1, 2, 3, 4};
 
         String result = gameProcess.guess(inputNumber);
-        Assertions.assertEquals(gameProcess.getTimes(), 6);
-        Assertions.assertEquals("1 2 4 3\t4A0B\n", result);
+        Assertions.assertEquals(6, gameProcess.getTimes());
+        Assertions.assertEquals("1 2 3 4\t4A0B\n", result);
+    }
+
+    @Test
+    public void should_return_end_when_times_change_to_6_given_1243_with_answer1234() {
+        int[] answer = {1, 2, 3, 4};
+        AnswerGenerator answerGenerator = Mockito.mock(AnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(answer);
+        GameProcess gameProcess = new GameProcess(answerGenerator);
+
+        int[] inputNumber = {1, 2, 4, 3};
+
+        String result1 = gameProcess.guess(inputNumber);
+        String result2 = gameProcess.guess(inputNumber);
+        String result3 = gameProcess.guess(inputNumber);
+        String result4 = gameProcess.guess(inputNumber);
+        String result5 = gameProcess.guess(inputNumber);
+        String result6 = gameProcess.guess(inputNumber);
+
+        Assertions.assertEquals(6, gameProcess.getTimes());
+        String result7 = gameProcess.guess(inputNumber);
+        Assertions.assertEquals(6, gameProcess.getTimes());
+        Assertions.assertEquals("end", result7);
     }
 }
