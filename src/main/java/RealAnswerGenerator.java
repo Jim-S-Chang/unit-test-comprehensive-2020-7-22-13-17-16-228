bf2dd93647bf7051ee930b3f0b38fc92009064f4
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-23 20:06:07 +0800

    RealAnswerGenerator [Jim] test:should_return_true_when_generate_answer_given_no_input_test_is_at_range bug-fix:change the loop end condition in RealAnswerGeneratorTest.should_return_true_when_generate_answer_given_no_input_test_is_repeated

--- src/main/java/RealAnswerGenerator.java
+++ src/main/java/RealAnswerGenerator.java
@@ -1,7 +1,7 @@
 public class RealAnswerGenerator implements AnswerGenerator{
     @Override
     public int[] generate() {
-        int[] answer = {1,3};
+        int[] answer = {1,3,2,10};
         return answer;
     }
 }
--- src/test/java/RealAnswerGeneratorTest.java
+++ src/test/java/RealAnswerGeneratorTest.java
@@ -12,6 +12,7 @@ public class RealAnswerGeneratorTest {
             for (int j = i + 1; j < answer.length; j++) {
                 if (answer[j] == answer[i]) {
                     result = false;
+                    break;
                 }
             }
         }
@@ -25,5 +26,21 @@ public class RealAnswerGeneratorTest {
         int[] answer = realAnswerGenerator.generate();
 
         Assertions.assertTrue(answer.length == 4);
+    }
+
+    @Test
+    public void should_return_true_when_generate_answer_given_no_input_test_is_at_range() {
+        RealAnswerGenerator realAnswerGenerator = new RealAnswerGenerator();
+        int[] answer = realAnswerGenerator.generate();
+
+        boolean result = true;
+        for (int i = 0; i < answer.length; i++) {
+            if (answer[i] < 0 || answer[i] > 9) {
+                result = false;
+                break;
+            }
+        }
+
+        Assertions.assertTrue(result);
     }
 }
