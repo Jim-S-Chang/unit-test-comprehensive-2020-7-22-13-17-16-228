e5fef80cdd1b96a0750c266132c03ec45552f2a6
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-27 21:12:40 +0800

    [GuessNumber][Jim]refactor:add List type

--- src/main/java/GuessNumber.java
+++ src/main/java/GuessNumber.java
@@ -8,15 +8,15 @@ public class GuessNumber {
         answer = answerGenerator.generate();
     }
 
-    public String guess(List inputGuess) {
+    public String guess(List<Integer> inputGuess) {
         return String.format("%dA%dB", getCountOfA(inputGuess), getCountOfB(inputGuess));
     }
 
-    private int getCountOfB(List inputGuess) {
+    private int getCountOfB(List<Integer> inputGuess) {
         return inputGuess.stream().filter(e->answer.contains(e)).toArray().length- getCountOfA(inputGuess);
     }
 
-    private int getCountOfA(List inputGuess) {
+    private int getCountOfA(List<Integer> inputGuess) {
         return IntStream
                 .range(0,inputGuess.size())
                 .filter(i->inputGuess.get(i)==answer.get(i))
